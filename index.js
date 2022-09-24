ec37e7eae75bcf8183be3800be3f582e04be7695
André Luferat <33375030+Luferat@users.noreply.github.com>
2022-09-24 10:18:45 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * JavaScript do aplicativo
+ * Depende de "jQuery" (https://jquery.com/)
+ *
+ * OBS: Este é o aplicativo principal, para que o tema (template) do site
+ * opere. Posteriormente, quando necessário, cada página (conteúdo) terá seu
+ * próprio JavaScript, assim, somente o JavaScript necessário será carregado.
+ *
+ * Para saber mais:
+ *   • https://www.w3schools.com/js/
+ *   • https://www.w3schools.com/jsref/
+ *   • https://www.w3schools.com/jquery/
+ */
+
+// Executa aplicativo JavaScript quando o documento estiver pronto
+$(document).ready(() => {
+  // Exibe uma mensagem no console do navegador
+  console.log("Blog IRS");
+});
